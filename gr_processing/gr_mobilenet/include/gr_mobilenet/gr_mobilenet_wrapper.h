74fd56cdd17cb8c48ad60dd91d90e62118f4a83c
Jose Mayoral <jose.carlos.mayoral.banos@nmbu.no>
2020-01-07 17:29:19 +0100

    Compiling OpenCV 3.4.1

--- gr_processing/gr_mobilenet/include/gr_mobilenet/gr_mobilenet_wrapper.h
+++ gr_processing/gr_mobilenet/include/gr_mobilenet/gr_mobilenet_wrapper.h
@@ -5,6 +5,7 @@
 #include <mutex>
 
 #include <opencv2/dnn.hpp>
+#include <opencv2/calib3d.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/core/utils/trace.hpp>
--- gr_processing/gr_mobilenet/src/gr_mobilenet_node.cpp
+++ gr_processing/gr_mobilenet/src/gr_mobilenet_node.cpp
@@ -1,9 +1,9 @@
-#include <opencv2/dnn.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/core/utils/trace.hpp>
-using namespace cv;
-using namespace cv::dnn;
+//#include <opencv2/dnn.hpp>
+//#include <opencv2/imgproc.hpp>
+//#include <opencv2/highgui.hpp>
+//#include <opencv2/core/utils/trace.hpp>
+//using namespace cv;
+//using namespace cv::dnn;
 #include <fstream>
 #include <iostream>
 #include <cstdlib>
