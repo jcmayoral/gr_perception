15588595b3f135f529de864049fda0f019e7e91f
jcmayoral <jose.carlos.mayoral.banos@nmbu.no>
2020-04-21 14:40:43 +0200

    small fix

--- feature_generation/rgb_2_thermal/main.py
+++ feature_generation/rgb_2_thermal/main.py
@@ -11,7 +11,7 @@ rgb_imgs, thermal_imgs = data_loader.load_samples(num_imgs=num_imgs,thermal_ext=
 
 r,c = num_imgs,2
 titles = ['Condition','Original']
-plt.figure(figsize=(5,5))
+#plt.figure(figsize=(5,5))
 fig, axs = plt.subplots(r, c,figsize=[20,20])
 cnt = 0
 for i in range(r):
