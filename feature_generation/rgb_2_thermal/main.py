82db174f75415db1fb21f6d3cf8ffc164e6b79a1
jcmayoral <jose.carlos.mayoral.banos@nmbu.no>
2020-04-22 09:42:47 +0200

    update script

--- feature_generation/rgb_2_thermal/main.py
+++ feature_generation/rgb_2_thermal/main.py
@@ -1,11 +1,16 @@
 from data_loader import DataLoader
 import matplotlib.pyplot as plt
 from tools import visualize
+from pix_2_pix import Pix2Pix
 
 num_imgs = 4
-
 data_loader = DataLoader("test", img_res=(128, 128),
              rgb_dataset_folder="/home/jose/Pictures",
              thermal_dataset_folder="/home/jose/Pictures")
 rgb_imgs, thermal_imgs = data_loader.load_samples(num_imgs=num_imgs,thermal_ext=".jpg")
-visualize(rgb_imgs, thermal_imgs)
+
+#TODO args
+if False:
+    visualize(rgb_imgs, thermal_imgs)
+
+model = Pix2Pix()
