52da533351598696f0c7e2013d2e1a1c64a02161
jcmayoral <jose.carlos.mayoral.banos@nmbu.no>
2020-05-20 16:37:22 +0200

    fusion available for multicameras... TESTING PENDING

--- fusion_components/gr_fused_detectors/src/ros_wrapper_node.cpp
+++ fusion_components/gr_fused_detectors/src/ros_wrapper_node.cpp
@@ -8,40 +8,31 @@ using namespace gr_detection;
 int main(int argc, char** argv){
     ros::init(argc, argv, "aaaa");
     DetectorsFuser detectors_fuser;
-
+    ros::NodeHandle nh("~");
     nodelet::Loader n(false);
-    ros::NodeHandle nh;
-    nodelet::Loader n2(false);
+    std::cout << nh.getNamespace() << std::endl;
 
-    ROS_ERROR("WIP...");
-        ros::Duration(10).sleep();
-
-    std::string name = "gr_depth_processing/MyNodeletClass";
+    std::string name = "/depth_nodelet";//ros::this_node::getNamespace();//ros::this;
     std::string type = "gr_depth_processing/MyNodeletClass";
 
     ros::M_string remappings = ros::names::getRemappings ();
-    std::vector<std::string> argvs;
+    remappings.insert(std::pair<std::string,std::string>("color_info","/color_info1"));
+    remappings.insert(std::pair<std::string,std::string>("depth_info","/depth_info1"));
+    remappings.insert(std::pair<std::string,std::string>("color_frame","/depth_frame1"));
+    remappings.insert(std::pair<std::string,std::string>("depth_frame","/depth_frame1"));
+    remappings.insert(std::pair<std::string,std::string>("bounding_boxes","/bounding_boxes1"));
 
-    
-    auto name2="gr_pointcloud_processing/PointCloudProcessor";
-    auto type2="gr_pointcloud_processing/PointCloudProcessor";
-    
-    std::cout << "RESULT : "<< n2.load(name2, type2, remappings, argvs) << std::endl;
+    std::vector<std::string> argvs;   
     std::cout << "RESULT : "<< n.load(name, type, remappings, argvs) << std::endl;
 
-    /*
-    FusionDetection* a = new FusionDetection();
-    Person p1;
-    FusionDetection* b = new FusionDetection();
-    std::cout << "insert  A "<<std::endl;
-    a->insertNewObject(p1);
-    std::cout << "show B"<<std::endl;
-    b->showCurrentDetections();
-    Person p2;
-    std::cout << "insert B"<<std::endl;
-    b->insertNewObject(p2);
-    std::cout << "show A"<<std::endl;
-    a->showCurrentDetections();
-    */
+    auto name2="/depth_nodelet_2";
+    remappings["color_info"] = "/color_info2";
+    remappings["depth_info"] = "/depth_info2";
+    remappings["color_frame"] = "/depth_frame2";
+    remappings["depth_frame"] = "/depth_frame2";
+    remappings["bounding_boxes"] = "/bounding_boxes2";
+    
+    std::cout << "RESULT : "<< n.load(name2, type, remappings, argvs) << std::endl;
+
     ros::spin();
 }
