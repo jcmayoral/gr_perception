e866e66113d75446a04fb644f597eefda540d292
jcmayoral <jose.carlos.mayoral.banos@nmbu.no>
2020-07-03 15:30:26 +0200

    persons stuff 0.0.6

--- gr_tools/persons_stuff/include/persons_stuff/persons_pcd_reader.h
+++ gr_tools/persons_stuff/include/persons_stuff/persons_pcd_reader.h
@@ -4,16 +4,40 @@
 #include <pcl_conversions/pcl_conversions.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <experimental/filesystem>
+#include <common_detection_utils/math_functions.hpp>
 
 namespace fs = ::boost::filesystem;
 
 namespace persons_stuff{
+
+    struct PersonInfo{
+        double mean_x;
+        double mean_y;
+        double mean_z;
+        double mean_i;
+        double var_x=0;
+        double var_y=0;
+        double var_z=0;
+        double var_i=0;
+        double rangex;
+        double rangey;
+        double rangez;
+        double rangei;
+    };
+
+    std::ostream& operator<<(std::ostream& os, const PersonInfo& pi){
+        return os<<pi.mean_x << "," <<pi.mean_y << "," << pi.mean_z << ", " << pi.mean_i << ", " <<
+        pi.var_x << "," <<pi.var_y << "," << pi.var_z << ", " << pi.var_i << ", " <<
+        pi.rangex << "," <<pi.rangey << "," << pi.rangez << ", " << pi.rangei  << '\n';
+    }
+
     class PersonsPCDReader{
         public:
         PersonsPCDReader();
         ~PersonsPCDReader();
         void readAllPCDFiles();
         void readBatchPCDFiles(int batch_size, int persons_per_batch=3);
+        PersonInfo evaluateCloud(const pcl::PointCloud<pcl::PointXYZI>::Ptr pc);
         private:
         ros::NodeHandle nh_;
         ros::Publisher pc_pub_;
--- gr_tools/persons_stuff/src/persons_pcd_reader.cpp
+++ gr_tools/persons_stuff/src/persons_pcd_reader.cpp
@@ -1,6 +1,7 @@
 #include <persons_stuff/persons_pcd_reader.h>
 
 using namespace persons_stuff;
+using namespace gr_detection;
 
 PersonsPCDReader::PersonsPCDReader(): nh_{"~"}{
      pc_pub_ = nh_.advertise<sensor_msgs::PointCloud2>("fake_pc", 1);
@@ -9,6 +10,39 @@ PersonsPCDReader::PersonsPCDReader(): nh_{"~"}{
 
 PersonsPCDReader::~PersonsPCDReader(){
     
+}
+
+PersonInfo PersonsPCDReader::evaluateCloud(const pcl::PointCloud<pcl::PointXYZI>::Ptr pc){
+
+    PersonInfo info;
+    std::vector<double> x_vector;
+    std::vector<double> y_vector;
+    std::vector<double> z_vector;
+    std::vector<double> i_vector;
+
+    for (int i = 0; i < pc->size() - 1; i++){
+        x_vector.push_back(pc->points[i].x);
+        y_vector.push_back(pc->points[i].y);
+        z_vector.push_back(pc->points[i].z);
+        i_vector.push_back(pc->points[i].intensity);
+    }
+
+    info.mean_x = calculateMean<double>(x_vector);
+    info.mean_y = calculateMean<double>(y_vector);
+    info.mean_z = calculateMean<double>(z_vector);
+    info.mean_i = calculateMean<double>(i_vector);
+
+    info.var_x = calculateVariance<double>(x_vector);
+    info.var_y = calculateVariance<double>(y_vector);
+    info.var_z = calculateVariance<double>(z_vector);
+    info.var_i = calculateVariance<double>(i_vector);
+
+    info.rangex = getAbsoluteRange<double>(x_vector);
+    info.rangey = getAbsoluteRange<double>(y_vector);
+    info.rangez = getAbsoluteRange<double>(z_vector);
+    info.rangei = getAbsoluteRange<double>(i_vector);
+
+    return info;
 }
 
 void PersonsPCDReader::readBatchPCDFiles(int batch_size, int persons_per_batch){
@@ -35,9 +69,15 @@ void PersonsPCDReader::readBatchPCDFiles(int batch_size, int persons_per_batch){
             ROS_ERROR_STREAM("Couldn't read file " << entry);
             continue;
         }
-        if (counter< persons_per_batch-1){
+
+        PersonInfo info;
+        info = evaluateCloud(cloud);
+        std::cout << info << std::endl;
+
+        if (counter<= persons_per_batch-1){
             ccloud += *cloud;
             counter++;
+            //ROS_INFO("APPEND PC");
             continue;
         }
         counter = 0;
@@ -60,6 +100,11 @@ void PersonsPCDReader::readAllPCDFiles(){
             ROS_ERROR_STREAM("Couldn't read file " << entry);
             continue;
         }
+
+        PersonInfo info;
+        info = evaluateCloud(cloud);
+        std::cout << info << std::endl;
+
         std::cout << "PUBLISHING "<<std::endl;
         pcl::toROSMsg(*cloud, output);
         output.header.frame_id = "velodyne";
