02e83b6f823d66dbe54e5602dee31b7c39863742
jcmayoral <jose.carlos.mayoral.banos@nmbu.no>
2020-09-09 14:37:28 +0200

    more useful script

--- gr_tools/persons_stuff/scripts/persons_heatmap.py
+++ gr_tools/persons_stuff/scripts/persons_heatmap.py
@@ -11,7 +11,7 @@ plt.ion()
 #fig, ax = plt.subplots(1,1,subplot_kw=dict(polar=True))
 fig = plt.figure()
 #ax = fig.add_subplot(111, projection='polar')
-ax = fig.add_axes([0.1,0.1,0.8,0.8],polar=True)
+ax = fig.add_axes([0.1,0.1,0.8,0.8])
 #global main_msg
 
 main_msg = FoundObjectsArray()
@@ -38,7 +38,7 @@ cmapdyn = get_cmap(20,'hsv')
 
 def plot():
     fig.canvas.flush_events()    #fig.canvas.draw()
-    plt.cla()
+    #plt.cla()
     global main_msg, indexes
     global cmap, cmapdyn
     #rospy.logwarn(indexes.objects)
@@ -61,20 +61,14 @@ def plot():
     ncount1 = 5
 
     for p in main_msg.objects:
-        nr = np.linalg.norm([p.pose.position.y, p.pose.position.x])
-        #r.append(nr)
-        narea = np.linalg.norm([p.speed.x, p.speed.y])*1000
-        #area.append(narea)
-        ntheta = np.arctan2(p.pose.position.y, p.pose.position.x)
-        #theta.append(ntheta)
+        x = p.pose.position.x
+        y = p.pose.position.y
+
         if p.is_dynamic:
             ncolor = cmapdyn(ncount1)
         else:
             ncolor = cmap(ncount1)
-        #else:
-        #colors.append(ncolor)
-        #    ncolor = 200
-        #legens.append(p.object_id)
+
         risk = "UNKNOWN"
         if p.object_id in items.keys():
             risk = " RISK:" +  str(items[p.object_id])
@@ -87,39 +81,16 @@ def plot():
                 riskmsg = "::DANGER"
             if items[p.object_id] < 0.2:
                 riskmsg = "::SAFE"
-        ax.scatter(ntheta, nr, c=ncolor, cmap="hot", s=narea+10.0, alpha=0.75, label=p.object_id+ riskmsg)
+        print x,y
+        ax.scatter(x, y)#, cmap="hot", s=narea+10.0, alpha=0.75, label=p.object_id+ riskmsg)
         #angle = euler_from_quaternion([p.pose.orientation.x, p.pose.orientation.y, p.pose.orientation.z, p.pose.orientation.w])[2]
-
-        """
-        wrap = lambda x: x if x > 0 else 0
-        print ntheta, type(ntheta), wrap(ntheta), type(ntheta)
-
-        plt.annotate(p.object_id+riskmsg,
-                    xy=(wrap(ntheta), nr),      # theta, radius
-                    xytext=(0.05, 0.05),
-                    xycoords='polar',
-                    textcoords='polar',
-                    arrowprops=dict(facecolor='black', shrink=0.05),
-                    horizontalalignment='center',
-                    verticalalignment='center',
-                    clip_on=True)
-        """
         ncount1 = ncount1 + 1
 
-    ax.set_ylim(0,15)
-    #ax.set_yticks(np.arange(-20,20,5.0))
-
-    legend = plt.legend(bbox_to_anchor=(1.05, 1), loc='upper center')
-    #legend = plt.legend()
-    for i in legend.get_texts():
-        if "DANGER" in i.get_text():
-            plt.setp(i, color='r')
-        if "SAFE" in i.get_text():
-            plt.setp(i, color='g')
-        if "WARNING" in i.get_text():
-            plt.setp(i, color='y')
+    print "a"
     fig.canvas.draw()
-    rospy.sleep(0.1)
+
+    #ax.set_ylim(0,15)
+    #rospy.sleep(0.1)
     #plt.close()
 
 
@@ -127,12 +98,21 @@ def plot():
 
 if __name__ == '__main__':
     rospy.init_node("persons_status_plotter")
-    subindexes = rospy.Subscriber("/safety_indexes", RiskIndexes,
-                            callback_indexes, queue_size =1)
+    #subindexes = rospy.Subscriber("/safety_indexes", RiskIndexes,
+    #                        callback_indexes, queue_size =1)
     sub = rospy.Subscriber("/pointcloud_lidar_processing/found_object", FoundObjectsArray,
                             callback, queue_size =1)
     while not rospy.is_shutdown():
         plot()
-
-
-    rospy.spin()
+    #rospy.spin()
+    print "out"
+    #plt.show()
+    ax.set_xticks(np.arange(-10, 10, 1))
+    ax.set_yticks(np.arange(-10, 10, 1))
+    plt.grid(linestyle='-', linewidth=2)
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    plt.title("All positions collected on recorded bags on sensor frame")
+    plt.savefig("heatmap_recordeddata.png")
+    rospy.sleep(10)
+    #ax.show()
