a34c34ef7f40908325c16eeeb8fb5cbcb40f6b2c
Jose Mayoral <jose.carlos.mayoral.banos@nmbu.no>
2020-10-29 17:05:57 +0100

    save figures

--- gr_tools/persons_stuff/scripts/persons_trajectories.py
+++ gr_tools/persons_stuff/scripts/persons_trajectories.py
@@ -2,7 +2,7 @@
 from safety_msgs.msg import FoundObjectsArray, RiskIndexes, RiskObject
 from geometry_msgs.msg import Twist
 from std_msgs.msg import Empty
-
+import time
 import rospy
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
@@ -14,8 +14,8 @@ from tf.transformations import euler_from_quaternion
 plt.ion()
 #fig, ax = plt.subplots(1,1,subplot_kw=dict(polar=True))
 fig = plt.figure()
-#ax = fig.add_subplot(111, projection='polar')
-ax = fig.add_axes([0.1,0.1,0.8,0.8])
+ax = fig.add_subplot(111)#, projection='polar')
+#ax = fig.add_axes([0.1,0.1,0.8,0.8])
 #global main_msg
 
 main_msg = FoundObjectsArray()
@@ -32,9 +32,10 @@ def callback(msg):
     global trajectories_x
     global trajectories_y
     #assume one person for now
-    p1 = msg.objects[0].pose
-    trajectories_x.append(p1.position.x)
-    trajectories_y.append(p1.position.y)
+    if len(msg.objects)>0:
+        p1 = msg.objects[0].pose
+        trajectories_x.append(p1.position.x)
+        trajectories_y.append(p1.position.y)
 
 def callback_indexes(msg):
     global indexes
@@ -46,31 +47,41 @@ def get_cmap(n, name='hsv'):
     return plt.cm.get_cmap(name, n)
 
 
-cmap = color=cm.rainbow(np.linspace(0,1,20))
+cmap = color=plt.cm.rainbow(np.linspace(0,1,22))
 #get_cmap(20,'binary')
 cmapdyn = get_cmap(20,'hsv')
+ready = False
 
 def new_trajectory(msg):
+    global ready
+    ready = True
+
+def plot():
+    global ready
+    if not ready:
+        #print "not finished"
+        return False
     global trajectories_x
     global trajectories_y
     global trajectory_id
 
-    fig.canvas.flush_events()    #fig.canvas.draw()
+    #fig.canvas.flush_events()    #fig.canvas.draw()
     #plt.cla()
     global main_msg, indexes
     global cmap, cmapdyn
-    trajectories
 
     narea=80
     ncount1 = 5
 
-    ax.plot(x, y,c=cmap[trajectory_id], alpha=0.75)
+    ax.plot(trajectories_x, trajectories_y,c=cmap[trajectory_id], label="trajectory_"+str(trajectory_id), alpha=0.75)
 
     trajectories_x = list()
     trajectories_y = list()
 
     trajectory_id = trajectory_id + 1
-    fig.canvas.draw()
+    #fig.canvas.draw()
+    ready = False
+    return True
 
 if __name__ == '__main__':
     rospy.init_node("persons_status_plotter")
@@ -79,14 +90,14 @@ if __name__ == '__main__':
     sub = rospy.Subscriber("/pointcloud_lidar_processing/found_object", FoundObjectsArray,
                             callback, queue_size =1)
     while not rospy.is_shutdown():
-        pass
-        #plot()
-    #rospy.spin()
+        if not plot():
+            time.sleep(0.1)
+    rospy.spin()
     print "out"
-    """
     plt.rc('font', size=20)
     plt.rc('axes', titlesize=40)
     plt.rc('axes', labelsize=20)
+    """
     #rect = patches.Rectangle((-1,-0.75),1.8,1.5)
     #ax.add_patch(rect)
     warnrect = np.array([[5.0,-5.0],[5.0,5.0],[-5.0,5.0],[-5.0,-5.0],[5.0,-5.0]])
@@ -98,17 +109,23 @@ if __name__ == '__main__':
 
     ax.set_xticks(np.arange(-8, 8, 1))
     ax.set_yticks(np.arange(-6, 6, 1))
+    """
     plt.xticks(fontsize=20, rotation=0)
     plt.yticks(fontsize=20, rotation=0)
     #plt.colorbar()#cmap, ax)
-    """
+
 
     plt.grid(linestyle='-', linewidth=2)
     plt.xlabel("Coordinate X [m]", fontsize=30)
     plt.ylabel("Coordinate Y [m]", fontsize=30)
-    plt.title("Persons Trajectories on Odometry Map")
-    #  plt.legend()
+    plt.title("Persons Trajectories on Odometry Frame")
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+    # Put a legend to the right of the current axis
+    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    #plt.legend()
     plt.savefig("trajectories_map.png")
     print "END"
+
     #rospy.sleep(10)
     #ax.show()
