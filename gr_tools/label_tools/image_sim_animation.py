00792dd8844f2f812cd911e3c5fa8f0e148ccff9
jcmayoral <jose.carlos.mayoral.banos@nmbu.no>
2021-01-23 11:29:29 +0100

    change interface

--- gr_tools/label_tools/image_sim_animation.py
+++ gr_tools/label_tools/image_sim_animation.py
@@ -12,6 +12,7 @@ class ImageSinAnimationLabeler:
         self.client = actionlib.SimpleActionClient('/darknet_ros/check_for_objects', CheckForObjectsAction)
         self.client.wait_for_server()
         rospy.loginfo("Server found")
+        self.image = Image()
         image_sub = Subscriber("/camera/color/image_raw", Image, queue_size=10)
         pos_sub = Subscriber("/animated_human/location", Vector3Stamped, queue_size=10)
         self.ats = ApproximateTimeSynchronizer([image_sub, pos_sub], queue_size=10, slop=1.0, allow_headerless=False)
@@ -22,13 +23,25 @@ class ImageSinAnimationLabeler:
         goal = CheckForObjectsActionGoal()
         goal.goal.id = 1
         goal.goal.image = image
+        self.image = image
         print ("Called")
-        self.client.send_goal(goal.goal)
-        result = self.client.wait_for_result(rospy.Duration.from_sec(5.0))
-        print("This is the result", result)
-        if result:
-            print (self.client.get_result()  )
+        self.client.send_goal(goal.goal,
+                            active_cb=self.callback_active,
+                            feedback_cb=self.callback_feedback,
+                            done_cb=self.callback_done)
+        self.client.wait_for_result(rospy.Duration.from_sec(120.0))
 
+    def callback_active(self):
+        rospy.loginfo("Goal has been sent to the action server.")
+
+    def callback_done(self,state, result):
+        #rospy.loginfo("Action server is done. State: %s, result: %s" % (str(state), str(result)))
+        rospy.loginfo("This is the result state %d "% state)
+        #if result:
+            #print (self.client.get_result())
+
+    def callback_feedback(feedback):
+        rospy.loginfo("Feedback:%s" % str(feedback))
 
 if __name__ == "__main__":
     ImageSinAnimationLabeler()
