f076a7392ae5b80ff39d91530d0f9a329b417162
jcmayoral <jose.carlos.mayoral.banos@nmbu.no>
2021-01-29 19:05:38 +0100

    working script

--- gr_tools/label_tools/fieldsafe_filter/pointcloud_fieldsafe_labeler.py
+++ gr_tools/label_tools/fieldsafe_filter/pointcloud_fieldsafe_labeler.py
@@ -4,6 +4,8 @@ from sensor_msgs.msg import PointCloud2
 import rospy
 import actionlib
 
+FILEPATH = "/media/jose/ADATA HD710/PHD/datasets/FIELDSAFE/pointcloud_example_2021-01-29-17-13-28.bag"
+
 class PCFieldSafeLabeler:
     def __init__(self):
         self.client = actionlib.SimpleActionClient('/pointcloud_lidar_processing/gr_pointcloud/process', GRPCProcessAction)
@@ -22,9 +24,7 @@ class PCFieldSafeLabeler:
                             feedback_cb=self.callback_feedback,
                             done_cb=self.callback_done)
         self.client.wait_for_result(rospy.Duration.from_sec(120.0))
-
-    def get_current_result(self):
-         self.client.get_result()
+        return self.client.get_result()
 
     def callback_active(self):
         rospy.loginfo("Goal has been sent to the action server.")
@@ -38,6 +38,32 @@ class PCFieldSafeLabeler:
 
 if __name__ == "__main__":
     rospy.init_node('pc_fieldsafe_label')
+    import os, sys
+    try:
+        os.mkdir("testdataset")
+    except:
+        pass
+        #sys.exit()
+    try:
+        os.chdir("testdataset")
+    except:
+        sys.exit()
+
+
+    import rosbag
+    bag = rosbag.Bag(FILEPATH)
     pclabeler = PCFieldSafeLabeler()
-    pclabeler.call(PointCloud2())
+    filename = ""
+    for topic, msg, t in bag.read_messages(topics=['/velodyne_points']):
+        rospy.sleep(0.05)
+        filename = str(msg.header.stamp.to_nsec())
+        current_result = pclabeler.call(msg)
+        with open(filename,'a') as f:
+            for detection in current_result.found_objects.objects:
+                f.write("%f %f %f "%(detection.pose.position.x, detection.pose.position.y, detection.pose.position.z))
+                f.write("%f %f %f %f "%(detection.pose.orientation.x, detection.pose.orientation.y, detection.pose.orientation.z,  detection.pose.orientation.w))
+                f.write("%f %f %f "%(detection.speed.x, detection.speed.y, detection.speed.z))
+                f.write("%f \n"%(detection.is_dynamic))
+
+    bag.close()
     #rospy.spin()
