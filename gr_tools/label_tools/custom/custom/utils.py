819def000468576e271d8448a33769d643e6ea70
jcmayoral <jose.carlos.mayoral.banos@nmbu.no>
2021-02-08 20:35:30 +0100

    matching depth and rgb frames

--- gr_tools/label_tools/custom/custom/utils.py
+++ gr_tools/label_tools/custom/custom/utils.py
@@ -1,6 +1,7 @@
 import rosbag
 import rospy
 from sensor_msgs.msg import CameraInfo
+from collections import OrderedDict
 
 def match_timestamps(rgb_stamps, depth_stamps):
     if len(rgb_stamps) < len(depth_stamps):
@@ -20,21 +21,47 @@ def extract_camera_info(bagfile, info_topic):
 def extract_timestamps_frombag(rbag, info_topic):
     stamps = []
     seqs = []
+    rtimes = []
+    stamps = []
+    start_time = None
+    rstart_time = None
     #with rosbag.Bag(bagfile, 'r') as rbag:
     msgs = rbag.read_messages(info_topic)
     #print outbag.get_type_and_topic_info()
     for i, m in enumerate(msgs):
         #print m[0], type(m[1])
+        if start_time is None:
+            start_time = m[1].header.stamp.to_sec()
+            rstart_time = m[2]
         seqs.append(m[1].header.seq)
-        stamps.append(m[1].header.stamp.to_nsec())
-    return seqs, stamps
+        stamps.append(m[1].header.stamp.to_sec()- start_time)
+        rtimes.append(m[2] - rstart_time)
+    return seqs, stamps, rtimes
 
 def extract_timestamps(bagfile, info_topic):
     stamps = []
+    start_time = None
+    rstart_time = None
+
     with rosbag.Bag(bagfile, 'r') as rbag:
         msgs = rbag.read_messages(info_topic)
         #print outbag.get_type_and_topic_info()
         for i, m in enumerate(msgs):
             #print m[0], type(m[1])
-            stamps.append(m[1].header.stamp.to_nsec())
+            if start_time is None:
+                start_time = m[1].header.stamp.to_sec
+                rstart_time = m[2]
+            stamps.append(m[1].header.stamp.to_sec()-start_time)
     return stamps
+
+def save_matches(matches):
+    with open("matches.txt", "w") as f:
+        for m in matches:
+            f.write("{} {} \n".format(m[0],m[1]))
+
+def stamps_to_dict(data):
+    mydict = dict()
+    for i in data:
+        seq, stamp, rt = i.rstrip().split(" ")
+        mydict[int(seq)] = stamp
+    return OrderedDict(sorted(mydict.items(), key=lambda t: t[0]))
--- gr_tools/label_tools/custom/custom_label_manager.py
+++ gr_tools/label_tools/custom/custom_label_manager.py
@@ -11,8 +11,8 @@ import cv2
 import rosbag
 import numpy as np
 from test_bb import plot_bbs
-from custom_dataset.utils import extract_camera_info, extract_timestamps, extract_timestamps_frombag
-
+from custom.utils import *
+import numpy as np
 
 def create_stamps_files(dbpath):
     bag =  rosbag.Bag(os.path.join(dbpath, "long_video" , "safecopy.bag"), 'r')
@@ -23,13 +23,33 @@ def create_stamps_files(dbpath):
     bag.close()
 
     with open("rgb_info.txt", "w") as f:
-        for seq, time in zip(rgb_info[0], rgb_info[1]):
-            f.write("{} {}\n".format(seq,time))
+        for seq, time,rtime in zip(rgb_info[0], rgb_info[1], rgb_info[2]):
+            f.write("{} {} {}\n".format(seq,time,rtime))
 
     with open("depth_info.txt", "w") as f:
-        for seq, time in zip(depth_info[0], depth_info[1]):
-            f.write("{} {}\n".format(seq,time))
+        for seq, time,rtime in zip(depth_info[0], depth_info[1], depth_info[2]):
+            f.write("{} {} {}\n".format(seq,time, rtime))
 
+def match_stamps(file1="rgb_info.txt", file2="depth_info.txt"):
+    rgb_data = stamps_to_dict(open(file1,'r'))
+    #rgb_data = sorted(rgb_data)
+    depth_data = stamps_to_dict(open(file2,'r'))
+    #depth_data = sorted(depth_data)
+    matches = []
+    for key, value in rgb_data.iteritems():
+        min_index = -1
+        val_minimum = 1000000000000000000
+        for key2, value2 in depth_data.iteritems():
+            time_diff = np.fabs(float(value)-float(value2))
+            if time_diff < val_minimum:
+                val_minimum = time_diff
+                min_index = key2
+        #print val_minimum
+        if min_index >0:
+            matches.append([key, min_index])
+
+    matches = sorted(matches)
+    save_matches(matches)
 
 def execute(dbpath):
     depth_camera_info = extract_camera_info(os.path.join(dbpath, "camera" , "camera_info.bag"), "/camera/depth/camera_info")
@@ -43,5 +63,6 @@ if __name__ == '__main__':
         sys.exit()
     if sys.argv[1] == "stamps":
         create_stamps_files(dbpath)
-
+    if sys.argv[1] == "match":
+        match_stamps()
     print "FINISH"
