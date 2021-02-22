302b5646ade3541194b9afce72b29d262910f5e5
jcmayoral <jose.carlos.mayoral.banos@nmbu.no>
2021-02-22 21:10:16 +0100

    new version

--- gr_tools/label_tools/custom/custom/image_processing.py
+++ gr_tools/label_tools/custom/custom/image_processing.py
@@ -2,6 +2,7 @@
 from darknet_ros_msgs.msg import CheckForObjectsAction, CheckForObjectsActionGoal, CheckForObjectsActionResult
 from sensor_msgs.msg import Image, CameraInfo
 from gb_visual_detection_3d_msgs.msg import BoundingBoxes3d
+from gr_action_msgs.msg import GRDepthProcessAction, GRDepthProcessActionGoal, GRDepthProcessActionResult
 import rospy
 import actionlib
 import copy
@@ -12,6 +13,7 @@ import tqdm
 import numpy as np
 import imutils
 from skimage import img_as_float
+import sys
 
 
 def rotateImage(image, angle):
@@ -26,15 +28,22 @@ class ImageProcessing(object):
     def __init__(self, matches, depth_camera_info):
         self.depth_camera_info = depth_camera_info
         self.distance = 2.0
+        #THIS IS DARKNET CLIENT
         self.client = actionlib.SimpleActionClient('/darknet_ros/check_for_objects', CheckForObjectsAction)
+        #THIS IS gr_depth_process client
+        self.gr_client = actionlib.SimpleActionClient('/gr_depth_process', GRDepthProcessAction)
+
         self.client.wait_for_server()
         rospy.loginfo("Darknet Server found")
+        self.gr_client.wait_for_server()
+        rospy.loginfo("GRDepthProcess Server found")
+
         self.odom_frame = "odom"
         self.image = Image()
         self.bbs = None
         self.img_pub = rospy.Publisher("/camera/depth/image_rect_raw2", Image, queue_size=1)
         self.depth_pub = rospy.Publisher("/camera/depth/camera_info", CameraInfo, queue_size=1)
-        self.bb_sub = rospy.Subscriber("/darknet_ros_3d/bounding_boxes", BoundingBoxes3d, self.bbs_cb)
+        #self.bb_sub = rospy.Subscriber("/darknet_ros_3d/bounding_boxes", BoundingBoxes3d, self.bbs_cb)
         self.cv_bridge = CvBridge()
 
     def bbs_cb(self, msg):
@@ -75,18 +84,22 @@ class ImageProcessing(object):
         depth_image.header.stamp = self.depth_camera_info.header.stamp = rospy.Time.now()
         depth_image.header.frame_id = self.depth_camera_info.header.frame_id = "camera_depth_optical_frame"
 
-        obbs = self.get_current_result()
+        #obbs is result of yolov3
+        darknet_bbs = self.client.get_result()
 
-        if obbs is None:
-            print "AAAAAAA"
+        if darknet_bbs is None:
+            #print "AAAAAAA"
             return
-        obbs = obbs.bounding_boxes.bounding_boxes
+
+        #obbs means original bounding boxes coming from detector
+        obbs = darknet_bbs.bounding_boxes.bounding_boxes
 
         if len(obbs) == 0:
             self.bbs = None
             return
 
         #print "publishing"
+        """
         for i in range(5):
             if self.bbs is not None:
                 if len(obbs) == len(self.bbs.bounding_boxes):
@@ -94,21 +107,45 @@ class ImageProcessing(object):
             self.img_pub.publish(depth_image)
             self.depth_pub.publish(self.depth_camera_info)
             rospy.sleep(0.2)
-
+        """
+        gr_goal = GRDepthProcessActionGoal()
+        print type(obbs)
+        gr_goal.goal.depth_image = depth_image
+        gr_goal.goal.bounding_boxes = darknet_bbs.bounding_boxes
+        gr_goal.goal.depth_info = self.depth_camera_info
+        self.gr_client.send_goal(gr_goal.goal)
+        success = self.gr_client.wait_for_result(rospy.Duration.from_sec(1.0))
+        if not success:
+            print "ERROR"
+            return
+        self.bbs = self.gr_client.get_result()
+        print "NEW BBS ", self.bbs, type(self.bbs)
         if self.bbs is None:
             return
 
+
+        """
+        for o_index, object in enumerate(self.bbs.found_objects.objects):
+            print o_index, object
+        print len(obbs), len(self.bbs.found_objects.objects)
         if len(obbs) != len(self.bbs.bounding_boxes):
             rospy.logwarn("error.. this will happen.. for now Skipping {} {}".format(len(obbs),len(self.bbs.bounding_boxes)))
             self.bbs = None
             return
+        """
 
-        for obb, bbs in zip(sorted(obbs), sorted(self.bbs.bounding_boxes)):
+        for obb, f_object in zip(obbs, self.bbs.found_objects.objects):
+            #print o_index, f_object
             if obb.Class != "person":
                 #print "Skipping " +bb.Class
                 continue
+            object_pose = f_object.pose
+            #print "Z DISTANCE ", object_pose.position.z
             height, width, channels = desired_shape
-            ring = min(3,int(bbs.zmin/self.distance))
+            ring = min(3,int(object_pose.position.z/self.distance))
+
+            if ring > 3 or ring <0:
+                ring = "ERROR"
             data = str(ring) + " "
             rx = obb.xmax - obb.xmin
             cx = float(rx/2+ obb.xmin)/width
@@ -127,6 +164,7 @@ class ImageProcessing(object):
             #pass
 
             label_filename = "image_{}.txt".format(index)
+            print "data: ", data
 
             with open(label_filename, "a+") as text_file:
                 text_file.write(data)
@@ -138,10 +176,6 @@ class ImageProcessing(object):
 
         #rospy.loginfo("Index {} obbs {} bbs {}".format(index,str(self.get_current_result()), str(self.bbs)))
         self.bbs = None
-
-
-    def get_current_result(self):
-        return self.client.get_result()
 
     def get_current_depth(self):
         return self.current_depth
