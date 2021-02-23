651cb7668b2b4175b746b4d3e963cae4d04477ea
jcmayoral <jose.carlos.mayoral.banos@nmbu.no>
2021-02-23 13:12:10 +0100

    debug

--- gr_tools/label_tools/custom/relabel.py
+++ gr_tools/label_tools/custom/relabel.py
@@ -13,20 +13,13 @@ def plot_bbs(image, bbs, require_new):
     y1 = int(np.rint((cy1 - cheight/2)*height))
     x2 = int(np.rint((cx1 + cwidth/2)*width))
     y2 = int(np.rint((cy1 + cheight/2)*height))
-    #print (x1,y1)
-    #print (x2,y2)
-    #print (cll, type(cll))
-    # Create a Rectangle patch
     cv2.rectangle(image, (x1, y1), (x2, y2), (255,0,0), 2)
     cv2.putText(image, str(int(cll)), (int(cx1*width),int(cy1*height)), cv2.FONT_HERSHEY_SIMPLEX,1, (0,0,255),2)
 
     cv2.imshow("TEST",image)
 
     if require_new:
-        #if key == "32":
-        #print "OK LLLLLL " + str(key)
-        #if key != 27 and key !=141:
-        cv2.putText(image, "WAIT FOR NEW CLASS" +str(int(cll)), (int(cx1*width),int(cy1*height)), cv2.FONT_HERSHEY_SIMPLEX,1, (255,0,255),2)
+        cv2.putText(image, "Replace ", (int(cx1*width),20+int(cy1*height)), cv2.FONT_HERSHEY_SIMPLEX,1, (255,0,255),2)
         cv2.imshow("TEST",image)
         key = cv2.waitKey(0)
         cv2.waitKey(1000)
@@ -47,6 +40,23 @@ def replace_line(file_name, line_nums, texts):
     out.writelines(lines)
     out.close()
 
+def get_closest_v2(detections, ccs,fl):
+    minval = 1000
+    if len(fl) <4:
+        return len(fl)
+    minindex = 0
+    for index,cc in enumerate(ccs):
+        print cc
+        score = np.sqrt(np.power(cc[0]-detections[3],2)+np.power(cc[1]-detections[4],2))
+        #score = np.fabs(cc-detections[0])
+        print score , "    SCORE "
+        if index not in fl:
+            if score < minval:
+                minval = score
+                minindex = index
+    return minindex
+
+
 def get_closest(detections,ccs):
     scores = []
     for cc in ccs:
@@ -62,6 +72,8 @@ def get_scores(detections,ccs):
         scores.append(np.sqrt(np.power(cc[0]-detections[1],2)+np.power(cc[1]-detections[2],2)))
     return scores
 
+def get_score(detections,cc):
+    return np.sqrt(np.power(cc[0]-detections[1],2)+np.power(cc[1]-detections[2],2))
 
 if __name__ == "__main__":
     filepath =  "/home/jose/datasets/dummy/dummy"
@@ -71,7 +83,7 @@ if __name__ == "__main__":
     print "OK"
 
     order_index = [-1,-1,-1,-1]
-    cc = [[0.5,0.5],[0.5,0.5],[0.5,0.5],[0.5,0.5]]
+    cc = [[1000,1000],[1000,1000],[1000,1000],[1000,1000]]
 
     if os.path.exists(filepath):
         images = open(filepath,'r')
@@ -87,6 +99,8 @@ if __name__ == "__main__":
         lastindex_file = open("lastindex.txt","w")
         skipped = open("skipped.txt","a")
 
+        nflags = []
+
         for img_index, img_filename in tqdm(enumerate(images)):
             if img_index <= start_index:
                 continue
@@ -99,60 +113,63 @@ if __name__ == "__main__":
 
             lindx = 0
 
-            with open(label_filename, "r") as fl:
+            #with  as fl:
             #for line in fileinput.input(label_filename, inplace=True):
-                #print line
-                replace_ind = []
-                replace_data = []
+            #print line
+            replace_ind = []
+            replace_data = []
+            labels = open(label_filename, "r").readlines()
 
-                for index, line in enumerate(fl):
-                    img = cv2.imread(img_filename.rstrip())
-                    label = [data for data in line.strip().split(" ")]#)
-                    detections = [float(d) for d in label]
+            for index, line in enumerate(labels):
+                img = cv2.imread(img_filename.rstrip())
+                label = [data for data in line.strip().split(" ")]#)
+                detections = [float(d) for d in label]
 
-                    flag = False
+                flag = False
+                lindx = index
 
-                    lindx = get_closest(detections, cc)
-                    print get_scores(detections,cc)
-                    print cc
-                    #print [np.fabs(detected_area - x) for x in areas]
-                    print "INDEX ", str(lindx)+"\n"
-                    print "index  ", lindx, " Expected ", order_index[lindx], " GOTTEN ", detections[0]
-                    print order_index
+                #if len(labels)>1:
+                #    lindx = get_closest_v2(detections, cc, nflags)
+                if lindx not in nflags:
+                    nflags.append(lindx)
+                #print "\n" + "NFLAGS", nflags
+                #print "\n" + "CCS ", cc
+                #print "\n" + "order ", order_index
+                #print "INDEX ", str(lindx)+"\n"
+                #print "index  ", lindx, " Expected ", order_index[lindx], " GOTTEN ", detections[0]
 
-                    if int(detections[0]) != order_index[lindx]:
-                        flag = True
+                if np.fabs(int(detections[0]) - order_index[lindx])>0 and get_score(detections, cc[lindx])>0.1:
+                    flag = True
+
+                #print detections[0]
+                #print "INDEX ", order_index
+
+
+                key = plot_bbs(img, detections, flag)
+
+                if key == None:
+                    order_index[lindx] = int(label[0])
+                    cc[lindx] = [float(detections[3]), float(detections[4])]
                     #lindx = lindx + 1
+                    continue
+                if key == 27:
+                    lastindex_file.close()
+                    sys.exit()
 
+                label[0] = key - 176
 
-                    key = plot_bbs(img, detections, flag)
+                order_index[lindx] = int(label[0])
+                cc[lindx] = [float(detections[3]), float(detections[4])]
 
-                    if key == None:
-                        order_index[lindx] = label[0]
-                        cc[lindx] = [detections[1], detections[2]]
-                        continue
-                    if key == 27:
-                        lastindex_file.close()
-                        sys.exit()
+                newlabel = ""
+                newlabel = newlabel.join([str(c)+ " " for c in label])[:-1]+"\n"
 
-                    label[0] = key - 176
+                replace_ind.append(index)
+                replace_data.append(newlabel)
 
-                    order_index[lindx] = label[0]
-                    cc[lindx] = [detections[1], detections[2]]
-
-                    newlabel = ""
-                    newlabel = newlabel.join([str(c)+ " " for c in label])[:-1]+"\n"
-
-                    replace_ind.append(index)
-                    replace_data.append(newlabel)
-
-                #print "Image index", img_index
                 lastindex_file.seek(0)
                 lastindex_file.write(str(img_index))
-                #print "indeces", replace_ind
-                #print "data ", replace_data
-                #print "replace"
-                replace_line(label_filename,replace_ind,replace_data)
+
+                #replace_line(label_filename,replace_ind,replace_data)
 
         lastindex_file.close()
-                #print newlabel.rstrip()
