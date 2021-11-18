5c6e887a3af35adf46341fccbcf46bc2a7759c45
Jose Mayoral <jocamaba1989@gmail.com>
2021-11-18 14:09:15 +0100

    create gr_ml repository

--- gr_ml/scripts/persons_pcd.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python
-from gr_ml.gr_person_stuff.read_pcd import PersonsPCDReader
-
-ppr = PersonsPCDReader()
--- gr_ml/src/gr_ml/gr_terrain_recognition/terrain_classifier.py
+++
@@ -1,6 +0,0 @@
-from gr_ml.gr_terrain_recognition.coder import   Features2Image
-import rospy
-
-class TerrainClassifier(Features2Image):
-    def __init__(self):
-        Features2Image.__init__(self,ros=True, pix_per_meter=2, msg_selection=2,meters=10, memory=100)#meters=10,pix_per_meter=1, msg_selection=2)
