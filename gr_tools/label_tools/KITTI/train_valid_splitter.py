dc66e43db670fde36ec294885a91685059b0ec20
Jose Mayoral <jocamaba1989@gmail.com>
2021-11-26 13:55:56 +0100

    new script

---
+++ gr_tools/label_tools/KITTI/train_valid_splitter.py
@@ -0,0 +1,58 @@
+import random
+import statistics
+import os
+
+
+def read_files():
+    d = dict()
+    for root,folder, files in os.walk("/home/jose/datasets/KITTI/training/image_02"):
+        if len(folder )>1:
+            print (folder)
+            for f in folder:
+                d[f] = [0,0,0,0]
+            continue
+
+        dclass = root.split("/")[-1]
+
+        for f in files:
+            if "png" in f:
+                continue
+            txtfile = os.path.join(root, f)
+            with open (txtfile, "r") as file:
+                for line in file:
+                    cl, x1, y1, w,h = line.rstrip().split(" ")
+                    d[dclass][int(cl)]+=1
+    return d
+
+def run_try(d):
+    totals = [0, 0, 0, 0]
+    runs_ids = list()
+    for fsample in range(random.randint(2, len(d.values()))):
+        b = [i for i in d.values()]
+        maxindex = len(b)
+        index = random.randint(0, maxindex - 1)
+        runs_ids.append(index)
+        totals[0] += b[index][0]
+        totals[1] += b[index][1]
+        totals[2] += b[index][2]
+        totals[3] += b[index][3]
+
+    stats = statistics.stdev(totals)
+    return runs_ids, totals, stats
+
+
+nclass_runs = read_files()
+
+ids = list()
+minstd = 100000
+best_soln = []
+best_totals = []
+
+for i in range(5):
+    sample, totals, stats = run_try(nclass_runs)
+    if stats < minstd:
+        best_soln = sample
+        minstd = stats
+        best_totals = totals
+
+print ("best ", best_soln, " best totals ", best_totals, " result " , minstd)
