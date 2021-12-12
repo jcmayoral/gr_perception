c47e14b6644a1d75704ed5e833bf4f7fc6435de2
Jose Mayoral <jocamaba1989@gmail.com>
2021-12-12 14:11:18 +0100

    filter entire file

--- gr_tools/label_tools/filter_down.py
+++ gr_tools/label_tools/filter_down.py
@@ -17,31 +17,38 @@ def load_labels(labels_folder='labels'):
             key2 = f.split(".")[0]
             if key1 not in labels:
                 labels[key1] = dict()
-            labels[key1][key2] = os.path.join(root, f)
+            labels[key1][key2] = os.path.join(root, f).rstrip()
     return labels
 
 labels_folder='/home/jose/datasets/KITTI/training/image_02/'
+#Load labels on validation data
 labels_dict=load_labels(labels_folder)
 
+filter_threshold = 15.0
+
+outputfilename = os.path.join("output", "filtered_valid.txt")
+outputfile = open(outputfilename, "a")
 #print(labels_dict, labels_folder)
 with open (sys.argv[1], 'r') as imgs:
     for i in imgs:
         index = i.rstrip().split("/")
         key1 = index[-2]
         key2 = index[-1].split(".")[0]
-        print(key1, key2)
         if key1 in labels_dict.keys():
-            print (key1, "found")
             if key2 in labels_dict[key1].keys():
-                print(key2, "found")
-                print(labels_dict[key1][key2])
-        continue
-        if index in labels_dict.keys():
-           # print('index', index)
-            lfile = labels_dict[index]
-            with open(lfile, 'r') as file:
-                for label_raw in file:
-                    cl, xc,yc,xw,yh = label_raw.rstrip().split(" ")
-                    #print(cl, i, "AAA")
+                lfile = labels_dict[key1][key2]
+
+                #If true store
+                flag = True
+                with open(lfile, 'r') as file:
+                    for label_raw in file:
+                        cl, xc,yc,xw,yh = label_raw.rstrip().split(" ")
+                        print(cl, i, "AAA")
+                        if float(cl)> filter_threshold:
+                            flag=False
+                if (flag):
+                    print("store", lfile, i)
+                    outputfile.write(i)
         continue
         img = cvw.imread(i)
+outputfile.close()
